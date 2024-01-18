1f2fb48cb4f4ebb407cb03fde88f346cc2aa3d50
gaexxx <gaespa@gmail.com>
2024-01-18 18:21:44 +0900

    get endpoint

--- 07-information-security/01-stock-checker/routes/api.js
+++ 07-information-security/01-stock-checker/routes/api.js
@@ -1,7 +1,63 @@
 "use strict";
 
+const mongoose = require("mongoose");
+
 module.exports = function (app) {
-  app.route("/api/stock-prices").get(function (req, res) {
-    res.send("ciao");
+  const Stock = mongoose.model("Stock", {
+    stock: { type: String },
+    likes: { type: Number },
+  });
+
+  app.route("/api/stock-prices/").get(async function (req, res) {
+    const { stock, like } = req.query;
+    console.log(stock, like);
+    let likes = 0;
+    if (like === "true") {
+      likes = 1;
+    }
+    try {
+      const foundStock = await Stock.findOne({ stock: stock });
+      if (!foundStock) {
+        let newStock = new Stock({
+          stock: stock,
+          likes: likes,
+        });
+        const savedStock = await newStock.save();
+        console.log(savedStock);
+        console.log("Stock saved");
+        // res.json({ stock: data.stock, title: data.title });
+      } else {
+        foundStock.likes += likes;
+        const savedStock = await foundStock.save();
+        console.log(savedStock);
+        console.log("Stock likes added");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    try {
+      const result = await fetch(
+        `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock.toUpperCase()}/quote`
+      );
+      if (!result.ok) {
+        const msg = `C'e stato un errore ${result.status} ${result.statusText}`;
+        throw new Error(msg);
+      }
+      const data = await result.json();
+      const foundStock = await Stock.findOne({ stock: stock });
+      if (!foundStock) {
+        console.log("no stock found");
+      }
+
+      res.json({
+        stockData: {
+          stock: stock.toUpperCase(),
+          price: data.latestPrice,
+          likes: foundStock.likes,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
   });
 };
