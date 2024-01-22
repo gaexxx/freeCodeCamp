080418ad08dfb1369235f426651eaced2a3bdd02
gaexxx <gaespa@gmail.com>
2024-01-22 23:43:37 +0900

    get, push, board.html

--- 07-information-security/02-anonymous-message-board/routes/api.js
+++ 07-information-security/02-anonymous-message-board/routes/api.js
@@ -1,9 +1,116 @@
-'use strict';
+"use strict";
+const mongoose = require("mongoose");
+const Thread = require("./mongooseThreadModel.js");
 
 module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+  app
+    .route("/api/threads/:board")
 
+    .post(async (req, res) => {
+      const { board } = req.params;
+      const { text, delete_password } = req.body;
+
+      const newThread = new Thread({
+        board,
+        text,
+        created_on: new Date(),
+        bumped_on: new Date(),
+        reported: false,
+        delete_password,
+        replies: [],
+      });
+
+      try {
+        const savedThread = await newThread.save();
+        res.json([savedThread]);
+      } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    })
+
+    // GET the most recent 10 bumped threads on the board
+    .get(async (req, res) => {
+      // in caso si voglia pulire la collection da tutti i documents
+      // Thread.deleteMany({}) // con {} si intende tutte le proprietà
+      //   .then((data) => {
+      //     console.log(data);
+      //     res.send("complete delete successful");
+      //   })
+      //   .catch((err) => console.log(err));
+      // return;
+      const { board } = req.params;
+
+      try {
+        const threads = await Thread.find({ board })
+          .sort({ bumped_on: "desc" })
+          .limit(10)
+          .select(
+            "-reported -delete_password -replies.reported -replies.delete_password"
+          )
+          .exec();
+
+        res.json(threads);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+  app
+    .route("/api/replies/:board")
+
+    // POST a new reply
+    .post(async (req, res) => {
+      const { board } = req.params;
+      const { text, delete_password, thread_id } = req.body;
+
+      try {
+        const foundThread = await Thread.findById(thread_id);
+
+        if (!foundThread) {
+          res.json({ error: "Thread not found" });
+          return;
+        }
+
+        foundThread.bumped_on = new Date();
+        foundThread.replies.push({
+          text,
+          created_on: foundThread.bumped_on,
+          delete_password,
+          reported: false,
+        });
+
+        const savedThread = await foundThread.save();
+
+        res.json([savedThread]);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    })
+
+    // GET a thread with all its replies
+    .get(async (req, res) => {
+      const { board } = req.params;
+      const { thread_id } = req.query;
+
+      try {
+        const thread = await Thread.findById(thread_id)
+          .select(
+            "-reported -delete_password -replies.reported -replies.delete_password"
+          )
+          .exec();
+
+        res.json(thread);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+  app.route("/:board/").get(function (req, res) {
+    res.sendFile(process.cwd() + "/views/board.html");
+  });
+  app.route("/:board/:threadid").get(function (req, res) {
+    res.sendFile(process.cwd() + "/views/thread.html");
+  });
 };
---
+++ 07-information-security/02-anonymous-message-board/routes/mongooseThreadModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Thread = mongoose.model("Thread", {
+  board: { type: String },
+  text: { type: String },
+  created_on: { type: Date },
+  bumped_on: { type: Date },
+  reported: { type: Boolean },
+  delete_password: { type: String },
+  replies: [
+    {
+      // _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+      text: { type: String },
+      created_on: { type: Date },
+      delete_password: { type: String },
+      reported: { type: Boolean },
+    },
+  ],
+});
+module.exports = Thread;
