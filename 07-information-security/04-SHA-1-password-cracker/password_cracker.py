d764922ac57f5ec4d17cc6862a5b7f6eaeb72f6c
gaexxx <gaespa@gmail.com>
2024-02-11 15:43:40 +0900

    tests passed

--- 07-information-security/04-SHA-1-password-cracker/password_cracker.py
+++ 07-information-security/04-SHA-1-password-cracker/password_cracker.py
@@ -1,10 +1,26 @@
 import hashlib
 
 def crack_sha1_hash(hash, use_salts=False):
-    file = open("top-10000-passwords.txt")
-    for word in file:
-        word = word.strip()
-        hashed_word = hashlib.sha1(word.encode()).hexdigest()
-        if hash == hashed_word:
-            return word
-            
+    if use_salts == False:
+        with open("top-10000-passwords.txt", "r") as pass_file: # with closes the file after each iteration
+            for word in pass_file:
+                word = word.strip()
+                hashed_word = hashlib.sha1(word.encode()).hexdigest()
+                if hash == hashed_word:
+                    return word
+            return "PASSWORD NOT IN DATABASE"
+
+    if use_salts == True:
+        with open("known-salts.txt", "r") as salt_file: # with closes the file after each iteration
+            for salt in salt_file:
+                salt = salt.strip()
+                with open("top-10000-passwords.txt", "r") as pass_file: # with closes the file after each iteration
+                    for word in pass_file:
+                        word = word.strip()
+                        salt_prepend = salt + word
+                        salt_append = word + salt
+                        hashed_word_sp = hashlib.sha1(salt_prepend.encode()).hexdigest()
+                        hashed_word_sa = hashlib.sha1(salt_append.encode()).hexdigest()
+                        if hash == hashed_word_sp or hash == hashed_word_sa:
+                            return word
+        return "PASSWORD NOT IN DATABASE"
