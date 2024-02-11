9fa69cc3f39b341cc5fddbcf8229a092d5827aaa
gaexxx <gaespa@gmail.com>
2024-02-11 12:56:23 +0900

    crack without salt

--- 07-information-security/04-SHA-1-password-cracker/password_cracker.py
+++ 07-information-security/04-SHA-1-password-cracker/password_cracker.py
@@ -1,4 +1,10 @@
 import hashlib
 
-# def crack_sha1_hash(hash):
-    
+def crack_sha1_hash(hash, use_salts=False):
+    file = open("top-10000-passwords.txt")
+    for word in file:
+        word = word.strip()
+        hashed_word = hashlib.sha1(word.encode()).hexdigest()
+        if hash == hashed_word:
+            return word
+            
